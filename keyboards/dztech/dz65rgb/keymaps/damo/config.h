2841045c7dffc0e2143f786108063846a44b7702
inetd404 <inetd404@users.noreply.github.com>
2026-07-13 21:32:28 +0800

    Add DZ65RGB V3 VIA keymap

---
+++ keyboards/dztech/dz65rgb/keymaps/damo/config.h
@@ -0,0 +1,9 @@
+// Copyright 2026 inetd404
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Retain the size reductions used by the former upstream VIA keymap.
+#undef ENABLE_RGB_MATRIX_PIXEL_FLOW
+#undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+#undef ENABLE_RGB_MATRIX_PIXEL_RAIN
